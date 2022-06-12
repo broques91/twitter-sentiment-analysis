21528b14207b96c9bd9a2a34bfa8672ab42565ab
broques91 <bastien.roques971@gmail.com>
2022-06-12 14:05:34 +0200

    clean code

--- produce/producer.py
+++ produce/producer.py
@@ -4,14 +4,11 @@ import logging
 from kafka import KafkaProducer
 import configparser
 import json
-import os
 
 
 def stream(data):
     i = 0
     for tweet in api.search_tweets(q=data, count=100, lang='fr'):
-        #print(tweet.text)
-        #print(tweet._json)
         producer.send(topic_name, tweet._json)
         i+=1
         if i == 100:
@@ -23,25 +20,27 @@ def stream(data):
 if __name__ == "__main__":
     config = configparser.ConfigParser()
 
-    # kafka
+    # Kafka
     producer = KafkaProducer(
         bootstrap_servers=['kafka:9092'],
         api_version=(0,11,5),
         value_serializer=lambda t: json.dumps(t).encode('utf-8')
         )
+
     topic_name = 'twitter'
 
     # Setup logging
     logging.basicConfig(
         level = logging.INFO,
         format = "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
-
+    
+    # Read credentials
     try:
-        config.read('config.ini')
-        consumer_key = config['twitter']['api_key']
-        consumer_secret = config['twitter']['api_key_secret']
-        access_token = config['twitter']['access_token']
-        access_token_secret = config['twitter']['access_token_secret']
+        config.read('secret.ini')
+        CONSUMER_KEY = config['twitter']['consumer_key']
+        CONSUMER_SECRET = config['twitter']['consumer_secret']
+        ACCESS_TOKEN = config['twitter']['access_token']
+        ACCESS_TOKEN_SECRET = config['twitter']['access_token_secret']
         logging.info("Twitter API credentials parsed.")
     except KeyError as e:
         logging.error("Config file not found. Make sure it is available in the directory.")
@@ -51,8 +50,8 @@ if __name__ == "__main__":
         exit()
 
     # authenticate
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
 
     # stream
