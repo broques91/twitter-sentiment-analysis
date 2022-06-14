a17cd2c6035b14d2350fdcb5b1b50d47f641c515
broques91 <bastien.roques971@gmail.com>
2022-06-14 11:54:15 +0200

    add date and clean tweet

--- consume/consumer.py
+++ consume/consumer.py
@@ -1,9 +1,32 @@
 import json
+import re
 from kafka import KafkaConsumer
 from pymongo import MongoClient
 from textblob import TextBlob
 from textblob import TextBlob
 from textblob_fr import PatternTagger, PatternAnalyzer
+
+
+def cleanTweet(tweet: str) -> str:
+    tweet = re.sub(r'http\S+', '', str(tweet))
+    tweet = re.sub(r'bit.ly/\S+', '', str(tweet))
+    tweet = tweet.strip('[link]')
+
+    # enlever utilisateur
+    tweet = re.sub('(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
+    tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
+
+    # enlever ponctuation
+    my_punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@â'
+    tweet = re.sub('[' + my_punctuation + ']+', ' ', str(tweet))
+
+    # enlever les nombres
+    tweet = re.sub('([0-9]+)', '', str(tweet))
+
+    # enlever les hashtag
+    tweet = re.sub('(#[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
+
+    return tweet
 
 
 def getSubjectivity(tweet: str) -> float:
@@ -43,32 +66,38 @@ consumer = KafkaConsumer(
 
 # Parse received data from Kafka
 for message in consumer:
-   record = json.loads(json.dumps(message.value))
-   #print(record)
-   content = record["text"]
-   print(content)
-   #created_at = record["created_at"]
-   #location = record["location"]
+    record = json.loads(json.dumps(message.value))
+    print(record)
+    print("========")
 
-   # output sentiment
-   polarity_value = getPolarity(content)
-   sentiment = getSentiment(polarity_value)
-   print("Sentiment: ", sentiment)
-   print("")
+    content = record["content"]
+    created_at = record["date"]
+    clean_tweet = cleanTweet(content)
 
-   # Create dictionary and ingest data into MongoDB
-   try:
-      tweet_rec = {
-        'content': content.encode('utf-8','strict'),
-        'sentiment': sentiment.encode('utf-8','strict'),
-        #'created_at': created_at.encode('utf-8','strict'),
-        #'location': content.encode('utf-8','strict'),
+    # TODO
+    # Format Date
+
+    # Predict sentiment
+    polarity_value = getPolarity(content)
+    sentiment = getSentiment(polarity_value)
+    print("Sentiment: ", sentiment)
+    print("")
+
+    # Create dictionary and ingest data into MongoDB
+    try:
+        tweet_rec = {
+            'date': created_at.encode('utf-8','strict'),
+            'content': content.encode('utf-8','strict'),
+            'clean_tweet': clean_tweet.encode('utf-8','strict'),
+            'sentiment': sentiment.encode('utf-8','strict'),
         }
-      rec_id1 = collection.insert_one(tweet_rec)
-      print("Data inserted with record ids", rec_id1)
-   
-   except Exception as e:
-       print(e)
-       print("Could not insert into MongoDB")
+        rec_id1 = collection.insert_one(tweet_rec)
+        print("Data inserted with record ids", rec_id1)
+
+    except Exception as e:
+        print(e)
+        print("Could not insert into MongoDB")
+
+    
 
 
