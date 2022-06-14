48a40f5797707b70e1365ed4707ea6f1f155cabd
broques91 <bastien.roques971@gmail.com>
2022-06-14 11:51:56 +0200

    add line chart

--- app/app.py
+++ app/app.py
@@ -32,7 +32,7 @@ def init_connection():
 
 # Pull data from the collection.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
+#@st.experimental_memo(ttl=600)
 def get_data():
     db = client.tweets
     items = db.elections.find()
@@ -41,7 +41,9 @@ def get_data():
 
 
 def create_df_from_list(items):
+    dates = []
     tweets = []
+    clean_tweets = []
     feelings = []
 
     # Remove _id
@@ -50,13 +52,16 @@ def create_df_from_list(items):
     
     # Print results.
     for item in items:
+        created_at = item['date'].decode('utf-8')
         tweet = item['content'].decode('utf-8')
+        clean_tweet = item['clean_tweet'].decode('utf-8')
         sentiment = item['sentiment'].decode('utf-8')
+        dates.append(created_at)
         tweets.append(tweet)
+        clean_tweets.append(clean_tweet)
         feelings.append(sentiment)
-        #st.write(f"{tweet}")
 
-    data = {'tweet':tweets,'sentiment':feelings}
+    data = {'date': dates, 'tweet':tweets, 'clean_tweet':clean_tweets, 'sentiment':feelings}
     # Create the pandas DataFrame
     df = pd.DataFrame(data)
     return df
@@ -64,20 +69,20 @@ def create_df_from_list(items):
 
 client = init_connection()
 
-items = get_data()
-
-df = create_df_from_list(items)
-
-
 while True:
-    # Create random col for test
-    df['random'] = np.random.randint(1, 6, df.shape[0])
 
-    # creating KPIs 
+    items = get_data()
+    df = create_df_from_list(items)
+    # Create random col for test
+    #df['random'] = np.random.randint(1, 6, df.shape[0])
+
+      # creating KPIs 
     count_tweets = len(df)
-    count_positive = int(df[(df["sentiment"]=='Positive')]['sentiment'].count() + np.random.choice(range(1,30)))
-    count_neutral = int(df[(df["sentiment"]=='Neutral')]['sentiment'].count() + np.random.choice(range(1,30)))
-    count_negative = int(df[(df["sentiment"]=='Negative')]['sentiment'].count() + np.random.choice(range(1,30)))
+    count_positive = len(df[df["sentiment"] == "Positive"])
+    count_neutral = len(df[df["sentiment"] == "Neutral"])
+    count_negative = len(df[df["sentiment"] == "Negative"])
+
+    df_pos = df[df["sentiment"] == "Positive"]
 
     avg_positive = count_positive / count_tweets
     avg_neutral = count_neutral / count_tweets
@@ -88,16 +93,16 @@ while True:
         kpi1, kpi2, kpi3, kpi4 = st.columns(4)
 
         # fill in those three columns with respective metrics or KPIs 
-        kpi1.metric(label="Tweets", value=int(count_tweets), delta= round(count_tweets) - 10)
-        kpi2.metric(label="Positive  😊", value=round(avg_positive), delta= round(avg_positive) - 10)
-        kpi3.metric(label="Neutral  😐", value=round(avg_positive), delta= round(avg_positive) - 10)
-        kpi4.metric(label="Negative  😒", value=round(avg_negative), delta= round(avg_positive) - 10)
+        kpi1.metric(label="Tweets", value=int(count_tweets), delta=None)
+        kpi2.metric(label="Positive  😊", value=count_positive, delta=avg_positive)
+        kpi3.metric(label="Neutral  😐", value=count_neutral, delta=avg_neutral)
+        kpi4.metric(label="Negative  😒", value=count_negative, delta=avg_negative)
 
 
         fig_col1, fig_col2 = st.columns(2)
         with fig_col1:
-            st.markdown("### First Chart")
-            fig = px.density_heatmap(data_frame=df, y='random', x='sentiment')
+            st.markdown("### Time Series")
+            fig = px.line(data_frame=df, x='date')
             st.write(fig)
         with fig_col2:
             st.markdown("### Second Chart")
