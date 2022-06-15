4f72a0427fedf3b97edd20fd39139fc54aa78b2e
broques91 <bastien.roques971@gmail.com>
2022-06-15 11:58:37 +0200

    add stream twitter api v2

--- produce/producer.py
+++ produce/producer.py
@@ -1,31 +1,55 @@
+import tweepy
+from tweepy import StreamingClient, StreamRule
+import os
 import json
-import logging
-import snscrape.modules.twitter as sntwitter
-
 from kafka import KafkaProducer
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bearer_token = os.getenv("bearer_token")
 
 
 # Kafka
 producer = KafkaProducer(
-    bootstrap_servers=['kafka:9092'],
-    api_version=(0,11,5),
-    value_serializer=lambda t: json.dumps(t, default=str).encode('utf-8')
+    bootstrap_servers=["kafka:9092"],
+    api_version=(0, 11, 5),
+    value_serializer=lambda t: json.dumps(t, default=str).encode("utf-8"),
 )
 
-topic_name = 'twitter'
+topic_name = "twitter"
 
-query = 'elections lang:fr -is:retweet'
-#query = 'elections lang:fr -is:retweet until-2022-06-01 since:2020-01-01'
-tweets = []
-limit = 100
 
-for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-    #print(vars(tweet))
-    data = vars(tweet)
-    #break
-    if len(tweets) == limit:
-        break
-    else:
-        # send to producer
+class TweetPrinterV2(tweepy.StreamingClient):
+    def on_tweet(self, tweet):
+        print(tweet.data)
+        print(f"{tweet.id} {tweet.created_at} ({tweet.author_id}): {tweet.text}")
+        data = tweet.data
         producer.send(topic_name, data)
-     
+        print("-" * 50)
+
+
+printer = TweetPrinterV2(bearer_token)
+
+# clean-up pre-existing rules
+rule_ids = []
+result = printer.get_rules()
+for rule in result.data:
+    print(f"rule marked to delete: {rule.id} - {rule.value}")
+    rule_ids.append(rule.id)
+
+if len(rule_ids) > 0:
+    printer.delete_rules(rule_ids)
+    printer = TweetPrinterV2(bearer_token)
+else:
+    print("no rules to delete")
+
+# add new rules
+# rule = StreamRule(value="Python")
+rule = StreamRule(value="elections lang:fr")
+printer.add_rules(rule)
+
+printer.filter(expansions="author_id", tweet_fields="created_at")
+
+printer.filter()
